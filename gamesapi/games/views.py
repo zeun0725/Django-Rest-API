4898ef82fde4c0eec6114ec3d1512e7ed146b068
parkjieun <wldmsdl3187@naver.com>
2021-07-15 00:28:34 +0900

    serializer

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -11,13 +11,16 @@ from games.serializers import GameSerializer
 # 내용을 Json으로 렌더링
 class JSONResponse(HttpResponse):
     def __init__(self, data, **kwargs):
+        # 수신된 데이터를 json으로 렌더링 후 반환된 바이트 문자열은 content에 저장
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
+        # 베이스 클래스의 초기자 호출
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
 @csrf_exempt
 def game_list(request):
+    # 모든 게임을 나열하거나 새 게임 생성
     if request.method == 'GET':
         games = Game.objects.all()
         games_serializer = GameSerializer(games, many=True)
@@ -33,6 +36,7 @@ def game_list(request):
 
 @csrf_exempt
 def game_detail(request, pk):
+    # 기존 게임을 검색, 업데이트, 삭제함
     try:
         game = Game.objects.get(pk=pk)
     except Game.DoesNotExist:
@@ -44,6 +48,7 @@ def game_detail(request, pk):
 
     elif request.method == 'PUT':
         game_data = JSONParser().parse(request)
+        # 기존 데이터(game_data)를 대체할 검색된 데이터로 인스턴스 생성
         game_serializer = GameSerializer(game, data=game_data)
         if game_serializer.is_valid():
             game_serializer.save()
