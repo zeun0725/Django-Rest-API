9d4d0e6debcd55ddeb639ba6b6024b44b48fe40d
parkjieun <wldmsdl3187@naver.com>
2021-07-15 00:46:50 +0900

    serializer

--- gamesapi/gamesapi/urls.py
+++ gamesapi/gamesapi/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^', include('games.urls')),
 ]
