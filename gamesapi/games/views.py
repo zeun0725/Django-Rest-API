52ee411e4476a6d5feb29dc701b5b6d76ec3e7b1
parkjieun <wldmsdl3187@naver.com>
2021-07-31 17:18:26 +0900

    permissions

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -2,9 +2,24 @@ from rest_framework.reverse import reverse
 from rest_framework import generics
 from rest_framework.response import Response
 from games.models import Game, GameCategory, PlayerScore, Player
-from games.serializers import GameSerializer, GameCategorySerializer, PlayerSerializer, PlayerScoreSerializer
+from games.serializers import GameSerializer, GameCategorySerializer, PlayerSerializer, PlayerScoreSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from games.permissions import IsOwnerOrReadOnly
 
 # 제네릭 클래스 기반 뷰의 활용
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-list'
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-detail'
+
 
 class ApiRoot(generics.GenericAPIView):
     name = 'api-root'
@@ -13,7 +28,8 @@ class ApiRoot(generics.GenericAPIView):
             'players': reverse(PlayerList.name, request=request),
             'game-categories': reverse(GameCategoryList.name, request=request),
             'games': reverse(GameList.name, request=request),
-            'scores': reverse(PlayerScoreList.name, request=request)
+            'scores': reverse(PlayerScoreList.name, request=request),
+            'users': reverse(UserList.name, request=request),
         })
 
 class GameCategoryList(generics.ListCreateAPIView):
@@ -32,12 +48,25 @@ class GameList(generics.ListCreateAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     name = 'game-list'
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
+
+    def perform_create(self, serializer):
+        # 요청으로 받은 사용자로 소유자를 설정하기 위해
+        # create 메서드에 추가적인 owner 필드를 전달함
+        serializer.save(owner=self.request.user)
 
 
 class GameDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     name = 'game-detail'
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    )
 
 
 class PlayerList(generics.ListCreateAPIView):
